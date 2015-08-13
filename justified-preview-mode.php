857e59c9c803c7554ae437d83e1f128efd34632c
Paul Hendrick <paul@errorstudio.co.uk>
2015-08-13 12:58:03 +0100

    initial commit - pretty much done :)

---
+++ justified-preview-mode.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: Justified CMS - Preview Mode
+Description: Check for a preview mode header and set the constant accordingly
+Version: 0.0.1
+Author: Error Studio
+Author URI: http://errorstudio.co.uk
+Plugin URI: http://errorstudio.co.uk
+Text Domain: justified-preview-mode
+*/
+
+function set_preview_mode(){
+    if(array_key_exists('HTTP_PREVIEW', $_SERVER)){
+        define("PREVIEW_MODE", $_SERVER['HTTP_PREVIEW']=="true" ? true : false);
+    }else {
+        define("PREVIEW_MODE", false);
+    }
+}
+add_action('init', 'set_preview_mode');
+
+?>
