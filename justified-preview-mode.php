e49d69b5e6b870638e06bbc754e9c6190fea501c
Paul Hendrick <paul@errorstudio.co.uk>
2015-08-25 13:19:58 +0100

    return a Custom_WP_Error response when trying to view a post which isn't published and the request is made with the preview:true header

--- justified-preview-mode.php
+++ justified-preview-mode.php
@@ -18,4 +18,49 @@ function set_preview_mode(){
 }
 add_action('plugins_loaded', 'set_preview_mode');
 
+add_action('rest_prepare_post', function($response){
+    global $post;
+
+    /*
+     * if the post in in anything but a published state, and we're NOT in preview mode,
+     * we should send back a response which mimics the WP_Error auth failed response
+     */
+
+    if($post->post_status != 'publish' && !JUSTIFIED_PREVIEW_MODE){
+        $response = new Custom_WP_Error('unauthorized', 'Authentication failed', array('status'=>403));
+    }
+
+    return $response;
+});
+
+/*
+ * WP_REST_Posts_Controller is expecting a response object with a 'link_header' function, but in the case of
+ * rendering an unpublished post when not in preview mode, we need to return a WP_Error, which we sub-class here
+ * and stubb the link_header method ourselves
+ */
+class Custom_WP_Error extends WP_Error {
+    public $data;
+    public $headers;
+    public $status;
+
+    public function link_header( $rel, $link, $other = array() ) {
+        $header = '<' . $link . '>; rel="' . $rel . '"';
+
+        foreach ( $other as $key => $value ) {
+            if ( 'title' === $key ) {
+                $value = '"' . $value . '"';
+            }
+            $header .= '; ' . $key . '=' . $value;
+        }
+        return $this->header( 'Link', $header, false );
+    }
+    public function header( $key, $value, $replace = true ) {
+        if ( $replace || ! isset( $this->headers[ $key ] ) ) {
+            $this->headers[ $key ] = $value;
+        } else {
+            $this->headers[ $key ] .= ', ' . $value;
+        }
+    }
+}
+
 ?>
